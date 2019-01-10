61292adf62bb77956dfca6f31b5e9426d1b9d23c
David Kim <davidjaeyunkim@yahoo.com>
2019-01-09 20:22:23 -0800

    Using environment variables

--- lib.py
+++ lib.py
@@ -1,1 +1,14 @@
-print("hi")
+import praw
+reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
+                     client_id='t28ADFT5o91xmA', client_secret="3ZPcOSR6laQys7nLGoyiy54fU5g",
+                     username='stockcraft', password='stockcraft')
+
+submission = reddit.submission(id='acqfgx')
+
+submission.comments.replace_more(limit=0)
+for top_level_comment in submission.comments:
+        print(top_level_comment.body)
+
+submission.comments.replace_more(limit=0)
+for comment in submission.comments.list():
+    print(comment.body)
