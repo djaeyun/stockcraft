6204af31a8e6c11a4e6cce2b8abb48b9cfdfd3ad
David Kim <davidjaeyunkim@yahoo.com>
2019-01-16 20:03:28 -0800

    Defined sentiment analysis in DF

--- lib.py
+++ lib.py
@@ -3,6 +3,14 @@ import pandas as pd
 pd.set_option('display.max_columns', 500)
 import os
 import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import subjectivity
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
 
 reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
                      client_id=os.environ["REDDIT_CLIENT_ID"], client_secret=os.environ["REDDIT_CLIENT_SECRET"],
@@ -45,7 +53,37 @@ def recognize_company(row):
 
 df["companies"] = df.apply(lambda row:recognize_company(row),axis=1)
 
+sentim_analyzer = SentimentAnalyzer()
+sid = SentimentIntensityAnalyzer()
+
 def sentiment_analysis(row):
         body = row.body
-
+        sentences = tokenize.sent_tokenize(body)
+        ratings = []
+        for sentence in sentences:
+                ss = sid.polarity_scores(sentence)
+                ratings.append(ss)
+        negatives = 0
+        positives = 0
+        neutrals = 0
+        for r in ratings:
+                negatives += r["neg"]
+                positives += r["pos"]
+                neutrals += r["neu"]
+        maxx = max([negatives, positives, neutrals])
+        if positives == maxx:
+                return "positive"
+        if negatives == maxx:
+                return "negative"
+        if neutrals == maxx:
+                return "neutral"
+        
 df["sentiment"] = df.apply(lambda row:sentiment_analysis(row),axis=1)
+df = df[df.companies.apply(lambda x:len(x) != 0)]
+
+#did stock rise or fall within a day of this comment
+#function that looks at each row, company, time, then figures out what stock price was 24 hrs later
+#then see if it's a rise/fall. stock access api to get
+#probably want to get more threads - all threads in past 2 weeks = "what are your moves tomorrow"
+#then get all the comments on those threads
+#try to generate stock list automatically - same api to get stock prices also get stock names
